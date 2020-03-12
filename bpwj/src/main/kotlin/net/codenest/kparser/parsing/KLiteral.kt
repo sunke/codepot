6dda41b9f40b550a83b2afba65be19ce0ea89c9b
aksun <alan.ke.sun@infor.com>
2020-03-12 19:34:16 +0100

    ignore case in KLiteral

--- bpwj/src/main/kotlin/net/codenest/kparser/parsing/KLiteral.kt
+++ bpwj/src/main/kotlin/net/codenest/kparser/parsing/KLiteral.kt
@@ -2,7 +2,7 @@ package net.codenest.kparser.parsing
 
 import net.codenest.kparser.lexing.KToken
 
-class KLiteral(val literal: String, level: Int = 0) : KTerminal("Literal", level) {
+class KLiteral(private val literal: String, private val ignoreCase: Boolean = false, level: Int = 0) : KTerminal("Literal", level) {
 
-    override fun qualify(token: KToken) = token.isWord() && literal == token.sval
+    override fun qualify(token: KToken) = token.isWord() && literal.equals(token.sval, ignoreCase)
 }
